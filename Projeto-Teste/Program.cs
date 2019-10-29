c6d401b66d6ae68841bee9f769665eca54f68d5c
Pedro Octavio <pedro2548@live.com>
2019-10-29 14:05:42 -0300

    Acrescentadas linhas!

--- Projeto-Teste/Program.cs
+++ Projeto-Teste/Program.cs
@@ -6,9 +6,11 @@ namespace Projeto_Teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("------------------------");
             Console.WriteLine("Ola Mundo!");
             Console.WriteLine("Como você está hoje????");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("------------------------");
             Console.ReadKey();
         }
     }
